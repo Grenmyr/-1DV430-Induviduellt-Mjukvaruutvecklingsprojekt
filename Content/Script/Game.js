209327d107c6e4b7e0b2a069982bcce9561ade8a
Grenmyr <dg222cs@student.lnu.se>
2014-04-14 17:09:18 +0200

    Har implementerat Story ID 9 tog 3 timmar längre än beräknat. Problem med gravitation.

--- Content/Script/Game.js
+++ Content/Script/Game.js
@@ -1,1 +1,96 @@
-﻿
+﻿var Init = function () {
+    var width = 800;
+    var height = 450;
+
+
+    var textureConstructor = Texture;
+    Texture = new textureConstructor();
+    var player = {
+        speed: 10, // movement in pixels per second
+        jumping: false,
+        x: 0,
+        y: 0,
+        sizey : 20,
+        sizex : 20,
+        gravity: 1
+        
+    }
+
+    var keyPressed = {
+        
+    };
+
+    addEventListener("keydown", function (e) {
+        
+        keyPressed[e.keyCode] = true;
+    }, false);
+
+    addEventListener("keyup", function (e) {
+        delete keyPressed[e.keyCode];
+    }, false);
+
+    var position = function (modifier) {
+       
+        if (38 in keyPressed) { // Player holding up
+            if (!player.jumping) {
+                player.jumping = true;
+                player.y += -100;
+            }
+        }
+        if (37 in keyPressed) { // Player holding left
+            player.x -= player.speed * modifier;
+        }
+        if (39 in keyPressed) { // Player holding right
+            
+            player.x += player.speed * modifier;
+            console.log(player.x);
+        }
+        if (40 in keyPressed) { // Player holding down
+            player.y += player.speed * modifier;
+        }
+        
+       
+        player.y += player.gravity;
+        
+        if (player.y >= 450 - player.sizey)
+        {
+            player.y = 450 - player.sizey;
+            player.jumping = false;   
+        }
+        if (player.x >= 800 - player.sizex ) {
+            player.x = 800 - player.sizex;
+        }
+        else if (player.x <= 0) {
+            player.x = 0;
+        }
+    }
+    
+    // This is used to adjust gamespeed if slow computor it get high modifier value in Position function.
+    var PrevTime = Date.now();
+    var main = function () {
+        var NewDate = Date.now();
+        var timeDiff = NewDate - PrevTime;
+
+        position(timeDiff / 100);
+        renderTexture();
+        PrevTime = NewDate;
+    };
+    
+    // Game loop, calling main function as fast as possible.
+    setInterval(main, 1);
+
+    // Function to reder units and texture.
+    var renderTexture = function () {
+ 
+        var backgroundTexture = Texture.Background();
+        var playerTexture = Texture.Player(player.x, player.y);
+        var enemyTexture = Texture.Enemy();
+    }
+
+}
+
+
+window.onload = function () {
+
+    Init();
+};
--- Content/Script/Texture.js
+++ Content/Script/Texture.js
@@ -1,1 +1,30 @@
-﻿
+﻿Texture = function () {
+        var canvas = document.createElement("canvas")
+        var ctx = canvas.getContext("2d");
+        canvas.width = 800;
+        canvas.height = 450;
+        canvas.setAttribute("id", "canvas")
+        document.body.appendChild(canvas);
+
+    this.Background = function () {
+        var backgroundImage = new Image();
+        backgroundImage.onload = function () {
+            ctx.drawImage(backgroundImage, 0, 0);
+        }
+        backgroundImage.src = "Content/Image/background.png";   
+    }
+    this.Player = function (xPos, yPos) {
+        var playerImage = new Image();
+        playerImage.onload = function () {
+            ctx.drawImage(playerImage, xPos, yPos);
+        }
+        playerImage.src = "Content/Image/unit.png";
+    }
+    this.Enemy = function () {
+        var enemyImage = new Image();
+        enemyImage.onload = function () {
+            ctx.drawImage(enemyImage, 100, 100);
+        }
+        enemyImage.src = "Content/Image/3.png";
+    }
+}
